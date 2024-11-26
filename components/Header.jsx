31edeb1cb5ded7d123e6fbecb97636aaefc977af
Mohammed-Am <mh.amouzoun@gmail.com>
2024-11-26 16:01:46 +0100

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+import '../src/index.css'
+export default function Header(){
+    return(
+        <nav>
+            <img src="Path.png" alt='global'/>
+            <p>my travel journal.</p>
+        </nav>
+    )
+}
