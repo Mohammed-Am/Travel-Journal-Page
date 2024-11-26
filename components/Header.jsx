d5521c1ada9d8008093ab0c525725a40243639fa
Mohammed-Am <mh.amouzoun@gmail.com>
2024-11-26 16:22:40 +0100

    add icons

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ import '../src/index.css'
 export default function Header(){
     return(
         <nav>
-            <img src="Path.png" alt='global'/>
+            <img src="/global.png" alt='global'/>
             <p>my travel journal.</p>
         </nav>
     )
