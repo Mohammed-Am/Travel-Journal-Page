e61bcf53214a7e6185b9901a9da90aad251bbc6d
Mohammed-Am <mh.amouzoun@gmail.com>
2024-11-30 14:30:14 +0100

    update css

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,9 @@
 import '../src/index.css'
 export default function Header(){
     return(
-        <nav>
+        <header>
             <img src="/images/global.png" alt='global'/>
             <p>my travel journal.</p>
-        </nav>
+        </header>
     )
 }
